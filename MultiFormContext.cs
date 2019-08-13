747eff2b98b0ee7bf0657d65237dee2d823e9a31
De oliveira, Arthur [CONBR] <ADeOliv3@its.jnj.com>
2019-08-12 22:18:21 -0300

    Removing unecessary code for showing for only one monitor

---
+++ MultiFormContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace pl.polidea.lab.Web_Page_Screensaver
+{
+    using System.Collections.Generic;
+
+    public class MultiFormContext : ApplicationContext
+    {
+        public MultiFormContext(List<Form> forms)
+        {
+            foreach (var form in forms)
+            {
+                form.FormClosed += (s, args) =>
+                {
+                    ExitThread();
+                };
+
+                form.Show();
+            }
+        }
+    }
+}
---
+++ View/ScreensaverForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Web_Page_Screensaver.Extension;
+
+namespace pl.polidea.lab.Web_Page_Screensaver
+{
+    public partial class ScreensaverForm : Form
+    {
+        private int currentSiteIndex = -1;
+
+        private readonly ScreensaverPreferences screensaverPreferences = new ScreensaverPreferences();
+
+        public ScreensaverForm()
+        {
+            SetGlobalEventHandler();
+
+            InitializeComponent();
+
+            Cursor.Hide();
+        }
+
+        private void SetGlobalEventHandler()
+        {
+            GlobalUserEventHandler userEventHandler = new GlobalUserEventHandler();
+            userEventHandler.Event += HandleUserActivity;
+        }
+
+        private void ScreensaverForm_Load(object sender, EventArgs e)
+        {
+            if (screensaverPreferences.Urls.Any())
+            {
+                if (screensaverPreferences.RandomizeUrls)
+                {
+                    screensaverPreferences.Urls = screensaverPreferences.Urls.Shuffle();
+                }
+
+                InitializeTimer();
+                ShowNextSite();
+            }
+            else
+            {
+                Close();
+            }
+        }
+
+        private void InitializeTimer()
+        {
+            Timer timer = new Timer
+            {
+                Interval = screensaverPreferences.RotateInterval * 1000
+            };
+            timer.Tick += (s, ee) => ShowNextSite();
+            timer.Start();
+        }
+
+        private void BrowseTo(string url)
+        {
+            webBrowser.Visible = true;
+            try
+            {
+                Debug.WriteLine($"Navigating: {url}");
+
+                webBrowser.Navigate(url);
+            }
+            catch
+            {
+                Debug.WriteLine($"An error ocurred trying to load the URL: {url}");
+            }
+        }
+
+        private void ShowNextSite()
+        {
+            currentSiteIndex++;
+
+            List<string> urls = screensaverPreferences.Urls;
+
+            if (currentSiteIndex >= urls.Count)
+            {
+                currentSiteIndex = 0;
+            }
+
+            BrowseTo(urls[currentSiteIndex]);
+        }
+
+        private void HandleUserActivity()
+        {
+            if (screensaverPreferences.CloseOnActivity)
+            {
+                Close();
+            }
+            else
+            {
+                closeButton.Visible = true;
+                Cursor.Show();
+            }
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
